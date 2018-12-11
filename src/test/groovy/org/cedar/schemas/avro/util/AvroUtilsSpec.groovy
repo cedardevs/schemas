8b5f2a5fd4ce212fee0324e3df0fb8fb44f01368
Evan McQuinn <evan.mcquinn@colorado.edu>
2018-12-11 15:48:35 -0700

    Fixing some namespace mistakes

--- src/test/groovy/org/cedar/schemas/avro/util/AvroUtilsSpec.groovy
+++ src/test/groovy/org/cedar/schemas/avro/util/AvroUtilsSpec.groovy
@@ -3,27 +3,24 @@ package org.cedar.schemas.avro.util
 import org.cedar.schemas.avro.psi.Input
 import org.cedar.schemas.avro.psi.Method
 import org.cedar.schemas.avro.psi.ParsedRecord
+import org.cedar.schemas.avro.psi.RecordType
 import spock.lang.Specification
 
 class AvroUtilsSpec extends Specification {
 
   def 'transforms an Input into a map'() {
-    def testInput = new Input(
-        method: Method.POST,
-        protocol: 'http',
-        host: 'localhost',
-        requestUrl: '/test',
-        source: 'test',
-        contentType: 'application/json',
-        content: '{"hello":"world"}'
-    )
+    def builder = Input.newBuilder()
+    builder.type = RecordType.granule
+    builder.method = Method.POST
+    builder.source = 'test'
+    builder.contentType = 'application/json'
+    builder.content = '{"hello":"world"}'
+    def testInput = builder.build()
 
     expect:
     AvroUtils.avroToMap(testInput) == [
+        type: RecordType.granule,
         method: Method.POST,
-        protocol: 'http',
-        host: 'localhost',
-        requestUrl: '/test',
         source: 'test',
         contentType: 'application/json',
         content: '{"hello":"world"}'
@@ -31,12 +28,11 @@ class AvroUtilsSpec extends Specification {
   }
 
   def 'ParsedRecord has empty array of errors by default'() {
-    def record = ParsedRecord.newBuilder().build()
+    def record = ParsedRecord.newBuilder().setType(RecordType.collection).build()
 
     expect:
     record.errors instanceof List
     record.errors.size() == 0
   }
-
 
 }
