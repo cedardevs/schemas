ca8020698d8ca256abdcb0a781c1259970b55620
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-05-24 10:44:07 -0600

    First pass at an mapToAvro capability and lenient json parsing using it

--- schemas-core/src/main/java/org/cedar/schemas/avro/util/AvroUtils.java
+++ schemas-core/src/main/java/org/cedar/schemas/avro/util/AvroUtils.java
@@ -1,24 +1,31 @@
 package org.cedar.schemas.avro.util;
 
+import org.apache.avro.JsonProperties;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificFixed;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AvroUtils {
+  static private Logger log = LoggerFactory.getLogger(AvroUtils.class);
 
   public static Map<String, Object> avroToMap(GenericRecord record) {
-    return avroToMap(record, false);
+    return avroToMap(record, true);
   }
 
   public static Map<String, Object> avroToMap(GenericRecord record, boolean recurse) {
@@ -55,14 +62,226 @@ public class AvroUtils {
     return GenericData.get().toString(record);
   }
 
-  public static <T extends GenericRecord> T jsonToAvro(String json, Schema schema) throws IOException {
+  public static <T extends GenericRecord> T jsonToAvro(String json, Schema schema) throws IOException, ClassNotFoundException {
+    return jsonToAvro(new ByteArrayInputStream(json.getBytes()), schema);
+  }
+
+  public static <T extends GenericRecord> T jsonToAvro(InputStream json, Schema schema) throws IOException, ClassNotFoundException {
     Decoder decoder = DecoderFactory.get().jsonDecoder(schema, json);
     return new SpecificDatumReader<T>(schema).read(null, decoder);
   }
 
-  public static <T extends GenericRecord> T jsonToAvro(InputStream json, Schema schema) throws IOException {
-    Decoder decoder = DecoderFactory.get().jsonDecoder(schema, json);
-    return new SpecificDatumReader<T>(schema).read(null, decoder);
+  public static <T extends GenericRecord> T jsonToAvroLenient(String json, Schema schema) throws IOException, ClassNotFoundException {
+    return jsonToAvroLenient(new ByteArrayInputStream(json.getBytes()), schema);
+  }
+
+  public static <T extends GenericRecord> T jsonToAvroLenient(InputStream json, Schema schema) throws IOException, ClassNotFoundException {
+    Map map = new ObjectMapper().readValue(json, Map.class);
+    return mapToAvro(map, findAvroClass(schema.getFullName()));
+  }
+
+  public static <T extends IndexedRecord> T mapToAvro(Map input, Class<T> avroClass) {
+    try {
+      T instance = avroClass.getDeclaredConstructor().newInstance();
+      Schema schema = instance.getSchema();
+      List<Schema.Field> fields = schema.getFields();
+      fields.forEach(f -> {
+        Object value = input.containsKey(f.name()) ? input.get(f.name()) : f.defaultVal();
+        instance.put(f.pos(), coerceValueForSchema(value, f.schema()));
+      });
+      return instance;
+    }
+    catch (Exception e) {
+      throw new IllegalStateException("Unable to build Avro object of type " + avroClass, e);
+    }
+  }
+
+  private static Object coerceValueForSchema(Object value, Schema schema) {
+    log.debug("coercing value [" + value + "] for schema [" + schema.getFullName() + "]");
+    switch(schema.getType()) {
+      case RECORD:
+        if (value instanceof Map) {
+          try {
+            Class<? extends IndexedRecord> recordClass = findAvroClass(schema.getFullName());
+            return mapToAvro((Map) value, recordClass);
+          }
+          catch(Exception e) {
+            throw new UnsupportedOperationException("Unable to build record of type " + schema.getFullName(), e);
+          }
+        }
+        break;
+
+      case ENUM:
+        try {
+          Class<? extends Enum> enumClass = findEnum(schema.getFullName());
+          if (enumClass.isAssignableFrom(value.getClass())) {
+            return value;
+          }
+          else {
+            return Enum.valueOf(enumClass, value.toString());
+          }
+        }
+        catch (Exception e) {
+          throw new UnsupportedOperationException("Unable to build enum of type " + schema.getFullName());
+        }
+
+      case ARRAY:
+        if (value instanceof List) {
+          Schema elementType = schema.getElementType();
+          return ((List) value).stream().map(v -> coerceValueForSchema(v, elementType)).collect(Collectors.toList());
+        }
+        break;
+
+      case MAP:
+        if (value instanceof Map) {
+          Schema valueType = schema.getValueType();
+          Map mapValue = (Map) value;
+          Map result = new LinkedHashMap();
+          mapValue.keySet().forEach(key -> result.put(key, coerceValueForSchema(mapValue.get(key), valueType)));
+          return result;
+        }
+        break;
+
+      case UNION:
+        for (Schema type : schema.getTypes()) {
+          try {
+            return coerceValueForSchema(value, type);
+          }
+          catch (Exception e) {
+            log.debug("tried and failed to coerce value for specific type " + type);
+          }
+        }
+
+      case FIXED:
+        if (value instanceof byte[]) {
+          try {
+            Class<? extends SpecificFixed> fixedClass = findFixed(schema.getFullName());
+            return fixedClass.getDeclaredConstructor(byte[].class).newInstance(value);
+          }
+          catch (Exception e) {
+            throw new UnsupportedOperationException("Unable to build fixed of type " + schema.getFullName());
+          }
+        }
+        break;
+
+      case STRING:
+        return value.toString();
+
+      case BYTES:
+        if (value instanceof byte[]) {
+          return value;
+        }
+        else if (value instanceof String) {
+          return ((String) value).getBytes();
+        }
+        break;
+
+      case INT:
+        if (value instanceof Integer) {
+          return value;
+        }
+        else if (value instanceof BigInteger) {
+          return ((BigInteger) value).intValueExact();
+        }
+        if (value instanceof String) {
+          return Integer.parseInt((String) value);
+        }
+        break;
+
+      case LONG:
+        if (value instanceof Long) {
+          return value;
+        }
+        else if (value instanceof Integer) {
+          return ((Integer) value).longValue();
+        }
+        else if (value instanceof BigInteger) {
+          return ((BigInteger) value).longValueExact();
+        }
+        else if (value instanceof String) {
+          return Long.parseLong((String) value);
+        }
+        break;
+
+      case FLOAT:
+        if (value instanceof Float) {
+          return value;
+        }
+        else if (value instanceof BigDecimal) {
+          Float result = ((BigDecimal) value).floatValue();
+          if (!result.equals(Float.NEGATIVE_INFINITY) && !result.equals(Float.POSITIVE_INFINITY)) {
+            return result;
+          }
+        }
+        else if (value instanceof String) {
+          return Float.parseFloat((String) value);
+        }
+        break;
+
+      case DOUBLE:
+        if (value instanceof Double) {
+          return value;
+        }
+        else if (value instanceof Float) {
+          return ((Float) value).doubleValue();
+        }
+        else if (value instanceof BigDecimal) {
+          Double result = ((BigDecimal) value).doubleValue();
+          if (!result.equals(Double.NEGATIVE_INFINITY) && !result.equals(Double.POSITIVE_INFINITY)) {
+            return result;
+          }
+        }
+        else if (value instanceof String) {
+          return Double.parseDouble((String) value);
+        }
+        break;
+
+      case BOOLEAN:
+        if (value instanceof Boolean) {
+          return value;
+        }
+        else if (value instanceof String) {
+          return Boolean.parseBoolean((String) value);
+        }
+        break;
+
+      case NULL:
+        if (value == null || value == JsonProperties.NULL_VALUE) {
+          return null;
+        }
+        break;
+    }
+    throw new UnsupportedOperationException("Unable to coerce value [" + value + "] of type [" + value.getClass() + "] for schema [" + schema.getFullName() + "]");
+  }
+
+  private static <T extends IndexedRecord> Class<T> findAvroClass(String className) throws ClassNotFoundException {
+    Class clazz = ClassLoader.getSystemClassLoader().loadClass(className);
+    if (IndexedRecord.class.isAssignableFrom(clazz)) {
+      return clazz;
+    }
+    else {
+      throw new IllegalArgumentException("Class " + className + " is not an Avro IndexedRecord class");
+    }
+  }
+
+  private static <T extends Enum> Class<T> findEnum(String className) throws ClassNotFoundException {
+    Class clazz = ClassLoader.getSystemClassLoader().loadClass(className);
+    if (clazz.isEnum()) {
+      return clazz;
+    }
+    else {
+      throw new IllegalArgumentException("Class " + className + " is not an enumeration");
+    }
+  }
+
+  private static <T extends SpecificFixed> Class<T> findFixed(String className) throws ClassNotFoundException {
+    Class clazz = ClassLoader.getSystemClassLoader().loadClass(className);
+    if (SpecificFixed.class.isAssignableFrom(clazz)) {
+      return clazz;
+    }
+    else {
+      throw new IllegalArgumentException("Class " + className + " is not a fixed");
+    }
   }
 
 }
--- schemas-core/src/test/groovy/org/cedar/schemas/avro/util/AvroUtilsSpec.groovy
+++ schemas-core/src/test/groovy/org/cedar/schemas/avro/util/AvroUtilsSpec.groovy
@@ -1,6 +1,9 @@
 package org.cedar.schemas.avro.util
 
 import groovy.json.JsonSlurper
+import org.cedar.schemas.avro.psi.Checksum
+import org.cedar.schemas.avro.psi.ChecksumAlgorithm
+import org.cedar.schemas.avro.psi.FileInformation
 import org.cedar.schemas.avro.psi.Input
 import org.cedar.schemas.avro.psi.Method
 import org.cedar.schemas.avro.psi.OperationType
@@ -23,15 +26,92 @@ class AvroUtilsSpec extends Specification {
     builder.operation = OperationType.NO_OP
     def testInput = builder.build()
 
-    expect:
-    AvroUtils.avroToMap(testInput) == [
-        type: RecordType.granule,
-        method: Method.POST,
-        source: 'test',
+    when:
+    def result = AvroUtils.avroToMap(testInput)
+
+    then:
+    result instanceof Map
+    result.type == RecordType.granule
+    result.content == '{"hello":"world"}'
+    result.contentType == 'application/json'
+    result.method == Method.POST
+    result.source == 'test'
+    result.operation == OperationType.NO_OP
+  }
+
+  def 'transforms a map into an input'() {
+    def inputMap = [
+        type       : RecordType.granule,
+        method     : Method.POST,
+        source     : 'test',
         contentType: 'application/json',
-        content: '{"hello":"world"}',
-        operation: OperationType.NO_OP
+        content    : '{"hello":"world"}',
+        operation  : OperationType.NO_OP
     ]
+
+    when:
+    def result = AvroUtils.mapToAvro(inputMap, Input)
+
+    then:
+    result instanceof Input
+    result.source == 'test'
+    result.contentType == 'application/json'
+    result.content == '{"hello":"world"}'
+  }
+
+  def 'transforms a map into a FileInformation'() {
+    def map = [
+        name: 'testfile',
+        size: 42,
+        format     : null,
+        //headers: null <-- intentionally excluded
+        checksums: [
+            [algorithm: 'MD5', value: 'abc'],
+            [algorithm: 'SHA1', value: 'xyz']
+        ],
+        metadata: [
+            "hello": "world",
+            "answer": "42"
+        ]
+    ]
+
+    when:
+    def result = AvroUtils.mapToAvro(map, FileInformation)
+
+    then:
+    result instanceof FileInformation
+    result.name == map.name
+    result.size == map.size
+    result.format == null
+    result.headers == null // <-- should have default value set
+    result.checksums instanceof List
+    result.checksums.size() == 2
+    result.checksums.every { it instanceof Checksum }
+    result.checksums[0] == Checksum.newBuilder().setAlgorithm(ChecksumAlgorithm.MD5).setValue('abc').build()
+    result.checksums[1] == Checksum.newBuilder().setAlgorithm(ChecksumAlgorithm.SHA1).setValue('xyz').build()
+    result.metadata instanceof Map
+    result.metadata.size() == 2
+    result.metadata['hello'] == map.metadata.hello
+    result.metadata['answer'] == map.metadata.answer
+  }
+
+  def 'leniently parses json into a FileInformation'() {
+    def json = '{"name":"testfile"}'
+
+    when:
+    def result = AvroUtils.jsonToAvroLenient(json, FileInformation.classSchema)
+
+    then:
+    result instanceof FileInformation
+    result.name == 'testfile'
+    // the rest should use their default values
+    result.size == 0
+    result.format == null
+    result.headers == null
+    result.checksums instanceof List
+    result.checksums.size() == 0
+    result.metadata instanceof Map
+    result.metadata.size() == 0
   }
 
   def 'ParsedRecord has empty array of errors by default'() {
@@ -61,7 +141,7 @@ class AvroUtilsSpec extends Specification {
 
   def 'builds avro objects from json #inputType'() {
     when:
-    def record = AvroUtils.<Publishing>jsonToAvro(input, Publishing.classSchema)
+    def record = AvroUtils.<Publishing> jsonToAvro(input, Publishing.classSchema)
 
     then:
     noExceptionThrown()
@@ -78,7 +158,7 @@ class AvroUtilsSpec extends Specification {
   def 'reads the example record from json'() {
     when:
     def inputStream = ClassLoader.systemClassLoader.getResourceAsStream('example-record-avro.json')
-    def inputRecord = AvroUtils.<ParsedRecord>jsonToAvro(inputStream, ParsedRecord.classSchema)
+    def inputRecord = AvroUtils.<ParsedRecord> jsonToAvro(inputStream, ParsedRecord.classSchema)
 
     then:
     inputRecord instanceof ParsedRecord
