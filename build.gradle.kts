8326fd9a269cd4c60a93257a716884400b1351f2
arianna <arianna.jakositz@colorado.edu>
2019-10-07 15:56:29 -0600

    jacoco reports generated locally; not sure why things are updated with codecov yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,17 +21,6 @@ subprojects {
                 }
             }
         }
-//        tasks.withType<JacocoReport> {
-//            executionData(fileTree(project.projectDir).include("build/jacoco/*.exec"))
-//
-//            reports {
-//                xml.isEnabled = true
-//                xml.destination = file("${project.buildDir}/reports/jacoco/report.xml")
-//                html.isEnabled = true
-//                html.destination = file("${project.buildDir}/reports/jacoco/html")
-//            }
-//            dependsOn(":test")
-//        }
 
         tasks.check {
             dependsOn(":jacocoTestReport")
