9e8db0e3de108762be62e412f89dfa22f12c3081
Erin <erin.rowland@colorado.edu>
2022-04-11 10:42:05 -0600

    Updated groovy & spock versions.

--- buildSrc/src/main/kotlin/utils.kt
+++ buildSrc/src/main/kotlin/utils.kt
@@ -6,9 +6,10 @@ object Versions {
     const val KAFKA: String = "2.3.0" // 3.1.0
     const val AVRO: String = "1.8.2"
 
-    const val GROOVY: String = "2.4.16"//"2.4.21"//"2.4.13" CVE
 //    const val APACHE_COMMONS = "1.6"
-    const val SPOCK: String = "1.1-groovy-2.4" //"1.3-groovy-2.5"
+
+    const val GROOVY: String = "3.0.9"
+    const val SPOCK: String = "2.0-M2-groovy-3.0"
     const val SLF4J = "1.7.30"
 
     const val COMMONS_TEXT = "1.6"
