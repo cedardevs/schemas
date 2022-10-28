d9de3dc7deb6766f4904f7b0911c8fe7739dc868
Jonathan Pelletier <jonathan.pelletier@noaa.gov>
2022-10-28 13:46:13 -0500

    Fix Kafka TestOuputTopic

--- schemas-core/src/test/groovy/org/cedar/schemas/avro/util/StreamSpecUtils.groovy
+++ schemas-core/src/test/groovy/org/cedar/schemas/avro/util/StreamSpecUtils.groovy
@@ -3,6 +3,7 @@ package org.cedar.schemas.avro.util
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.TestOutputTopic
 import org.apache.kafka.streams.TopologyTestDriver
 
 class StreamSpecUtils {
@@ -17,7 +18,9 @@ class StreamSpecUtils {
       Deserializer keyDeserializer = STRING_DESERIALIZER, Deserializer valueDeserializer = AVRO_DESERIALIZER) {
     def curr
     def output = []
-    while (curr = driver.readOutput(topic, keyDeserializer, valueDeserializer)) {
+    TestOutputTopic outputTopic = driver.createOutputTopic(topic, keyDeserializer, valueDeserializer)
+    while (!outputTopic.isEmpty()) {
+      curr = outputTopic.readRecord()
       output << curr
     }
     return output
