707f01cc1e8c67856296b8005311ebaae5a887fc
Jonathan Pelletier <jonathan.pelletier@noaa.gov>
2022-12-23 10:03:26 -0600

    Update Confluent
    Set the Kafka version to resolved Confluent to resolve inconsistant
    versions.

--- buildSrc/src/main/kotlin/utils.kt
+++ buildSrc/src/main/kotlin/utils.kt
@@ -2,8 +2,8 @@
 object Versions {
     const val DEFAULT: String = "0.0.0"
 
-    const val CONFLUENT: String = "7.1.3"
-    const val KAFKA: String = "3.1.2"
+    const val CONFLUENT: String = "7.1.5"
+    const val KAFKA: String = "${CONFLUENT}-ccs" //"3.1.2"
     const val AVRO: String = "1.11.1"
 
 //    const val APACHE_COMMONS = "1.6"
